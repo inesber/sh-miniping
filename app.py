c775f79a2026163ef7336aa7c00dcb21cce41091
inesber <55463738+inesber@users.noreply.github.com>
2023-01-31 18:13:53 +0000

    Created base html pages

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
-from flask import Flask
+from flask import Flask, render_template
+import random
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, cdcdd!</p>"
+def home():
+    color = random.choice(["red", "yellow", "blue"])
+    return render_template("home.html", color=color)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
