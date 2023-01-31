9c59cb1d519c25c316cc015906c952403f101007
inesber <55463738+inesber@users.noreply.github.com>
2023-01-31 19:06:36 +0000

    Added style to pages

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app = Flask(__name__)
@@ -7,9 +7,26 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     color = random.choice(["red", "yellow", "blue"])
-    return render_template("home.html", color=color)
+
+    panels = [
+        {"title": "Netflix", "url": "netflix.com"},
+        {"title": "Youtube", "url": "youtube.com"},
+        {"title": "Goodreads", "url": "goodreads.com"},
+        {"title": "Duolingo", "url": "duolingo.com"}
+    ]
+
+    return render_template("home.html", color=color, panels=panels)
 
 
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+
+@app.route("/ping")
+def ping():
+    return {
+        "url": "https://www.netflix.com/browse",
+        "code": "200",
+        "speed": 400
+    }
